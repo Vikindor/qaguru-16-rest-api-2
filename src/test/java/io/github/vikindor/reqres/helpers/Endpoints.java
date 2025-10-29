2a79c2f594b7e2a9dc080a406d2e302285bc5615
Vikindor <vikindor@protonmail.com>
2025-10-29 20:46:52 +0300

    refactor: remove unused endpoints

--- src/test/java/io/github/vikindor/reqres/helpers/Endpoints.java
+++ src/test/java/io/github/vikindor/reqres/helpers/Endpoints.java
@@ -3,6 +3,4 @@ package io.github.vikindor.reqres.helpers;
 public class Endpoints {
     public static final String USERS = "/users";
     public static final String SINGLE_USER = "/users/{id}";
-    public static final String LOGIN = "/login";
-    public static final String REGISTER = "/register";
 }
