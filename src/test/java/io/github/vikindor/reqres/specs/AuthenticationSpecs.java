acbc56739c351563815d9721fcb04a8a57fdc9ef
Vikindor <vikindor@proton.me>
2025-11-07 15:33:06 +0300

    refactor: remove unused imports

--- src/test/java/io/github/vikindor/reqres/specs/AuthenticationSpecs.java
+++ src/test/java/io/github/vikindor/reqres/specs/AuthenticationSpecs.java
@@ -1,13 +1,9 @@
 package io.github.vikindor.reqres.specs;
 
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 
 import static io.github.vikindor.reqres.helpers.CustomAllureListener.withCustomTemplate;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.BODY;
-import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 
 public class AuthenticationSpecs {
